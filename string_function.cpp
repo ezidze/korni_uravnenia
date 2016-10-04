fc2b7e5f54770b3eedf23e4aefde55561fdcf315
Maxim Raikovskyi <maksim.raykovskiy@phystech.edu>
2016-10-04 21:46:42 +0000

    comment

---
+++ string_function.cpp
@@ -0,0 +1,11 @@
+#include <stdio.>
+#include<stdlib.h>
+#include<assert.h>
+
+int strlen(char link){
+   assert(link);
+   int numerator = 0;
+   while ( link[numerator] != "\0") ++numerator;  
+}
+
+
